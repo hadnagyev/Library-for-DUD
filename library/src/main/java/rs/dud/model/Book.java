42546d93a0f8fe41631a9121548e03704ce4c22b
Damir Hadnagyev <33582312+hadnagyev@users.noreply.github.com>
2017-12-19 18:49:42 +0000

    book model done

--- library/src/main/java/rs/dud/model/Book.java
+++ library/src/main/java/rs/dud/model/Book.java
@@ -1,8 +1,22 @@
 package rs.dud.model;
 
-public class Book {
-	int id; //redni broj in excel file
-	int inventoryNumber; //inv. broj in excel file
-	String publisherName; //Izdavac in excel file
+import java.time.Year;
 
+public class Book {
+	int id; 						//redni broj in excel file
+	int inventoryNumber; 			//inv. broj 
+	String publisherName; 			//Izdavac 
+	Year yearOfPublishing;			//godina izdavanja 
+	String edition;					//edicija 
+	String nameOfWriterOriginal; 	// ime pisca izvorno 
+	String writer;					//pisac 
+	String originalTitle; 			// naslov originala 
+	String title;					//naslov 
+	String language;				//jezik 
+	String writingSystem;			//pismo (cyrylic, latin etc.)
+	String Genre;					//zanr
+	String bookCondition;			//stanje knjige
+	String bookOrigin;				//poreklo knjige
+	String bookLocation;			//gde se nalazi
+	
 }
