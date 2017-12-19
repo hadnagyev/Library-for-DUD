fd1893ae2872a75f8a0dc90ce0d57f6136655aed
Damir Hadnagyev <33582312+hadnagyev@users.noreply.github.com>
2017-12-19 18:35:39 +0000

    Book added to model

---
+++ library/src/main/java/rs/dud/model/Book.java
@@ -0,0 +1,8 @@
+package rs.dud.model;
+
+public class Book {
+	int id; //redni broj in excel file
+	int inventoryNumber; //inv. broj in excel file
+	String publisherName; //Izdavac in excel file
+
+}
