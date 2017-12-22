569a7ed032688e8ef5d86702000c7b1aca9ed0ad
Damir Hadnagyev <33582312+hadnagyev@users.noreply.github.com>
2017-12-22 22:51:28 +0000

    populating arraylist from txt file - done

--- library/src/main/java/main/Application.java
+++ library/src/main/java/main/Application.java
@@ -1,29 +1,112 @@
 package main;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
-import rs.dud.model.Library;
+import rs.dud.model.Book;
 
 public class Application {
+	ArrayList<String> readFromFile = new ArrayList<String>();
+	ArrayList<Book> library = new ArrayList<Book>();
+	String file = "POPIS KNJIGA najnoviji.txt";
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		ArrayList<String> libraryAddress = new ArrayList<String>();
-		String address;
-		System.out.println("Enter library owner");
-		String libraryOwner = keyboard.nextLine();
-		System.out.println("Enter library address");
+	public static void main(String[] args) throws IOException, UnsupportedEncodingException {
+		Application app = new Application();
+		app.readFromFile();
+		app.fillBooksToLibrary(app.findFirstEntry());
 
-		while (!(address = keyboard.nextLine()).isEmpty()) {
-			libraryAddress.add(address);
-			System.out.println("Enter additional address, if finished press enter on blank");
+		Scanner sc = new Scanner(System.in);
 
+		int izbor = sc.nextInt();
+		while (izbor != 0) {
+			
 		}
-		keyboard.close();
-		Library library = new Library(libraryOwner, libraryAddress);
-		System.out.println(library);
-
+		sc.close();
 	}
 
+	// populate arraylist from initial arraylist to arraylist with book model
+	private void fillBooksToLibrary(int indexOfFirstEntry) {
+		while (indexOfFirstEntry != readFromFile.size()) {
+			String idInput = readFromFile.get(indexOfFirstEntry++);
+			String idNumber = idInput.substring(0, idInput.length() - 2);
+
+			Integer id = Integer.valueOf(idNumber);
+			String inventoryNumberInput = readFromFile.get(indexOfFirstEntry++);
+			String inventoryNumberValue;
+			if (inventoryNumberInput.equals("")) {
+				inventoryNumberValue = "0";
+			} else {
+				inventoryNumberValue = inventoryNumberInput.substring(0, inventoryNumberInput.length() - 2);
+			}
+
+			int inventoryNumber = Integer.parseInt(inventoryNumberValue);
+			String publisherName = readFromFile.get(indexOfFirstEntry++);
+
+			Year yearOfPublishing;
+			if (readFromFile.get(indexOfFirstEntry).equals("")) {
+				yearOfPublishing = null;
+				indexOfFirstEntry++;
+			} else {
+				yearOfPublishing = Year.parse(readFromFile.get(indexOfFirstEntry++));
+			}
+
+			String edition = readFromFile.get(indexOfFirstEntry++);
+			String nameOfWriterOriginal = readFromFile.get(indexOfFirstEntry++);
+			String writer = readFromFile.get(indexOfFirstEntry++);
+			String originalTitle = readFromFile.get(indexOfFirstEntry++);
+			String title = readFromFile.get(indexOfFirstEntry++);
+			String language = readFromFile.get(indexOfFirstEntry++);
+			String writingSystem = readFromFile.get(indexOfFirstEntry++);
+			String genre = readFromFile.get(indexOfFirstEntry++);
+			String bookCondition = readFromFile.get(indexOfFirstEntry++);
+			String bookOrigin = readFromFile.get(indexOfFirstEntry++);
+			String bookLocation = readFromFile.get(indexOfFirstEntry++);
+			if (readFromFile.get(indexOfFirstEntry).equals("")) {
+				indexOfFirstEntry++;
+			}
+
+			Book book = new Book(id, inventoryNumber, publisherName, yearOfPublishing, edition, nameOfWriterOriginal,
+					writer, originalTitle, title, language, writingSystem, genre, bookCondition, bookOrigin,
+					bookLocation);
+			library.add(book);
+			System.out.println(book.toString());
+			if (readFromFile.get(indexOfFirstEntry).equals("R.br.")) {
+				break;
+			}
+		}
+	}
+
+	// populate arraylist from txt file
+	private void readFromFile() {
+		try (Stream<String> stream = Files.lines(Paths.get(file))) {
+			stream.forEach(readFromFile::add);
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Unsuported encoding");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// finding first entry in the txt file (starts with "1. " when exporting
+	// from .doc to .txt using openoffice
+	private int findFirstEntry() {
+		int indexOfFirstEntry = 0;
+		String line;
+		outerloop: while (indexOfFirstEntry != readFromFile.size()) {
+			line = readFromFile.get(indexOfFirstEntry);
+			System.out.println(indexOfFirstEntry);
+			if (line.equals("1. ")) {
+				break outerloop;
+			}
+			indexOfFirstEntry++;
+		}
+		System.out.println(indexOfFirstEntry);
+		return indexOfFirstEntry;
+	}
 }
